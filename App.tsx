9f8b18739b339bacd5fcbd60ab26929a2b45be3b
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-07-19 19:04:04 +0530

    UI Color

--- App.tsx
+++ App.tsx
@@ -6,44 +6,38 @@ import { CountryList } from "./components/countryList";
 import { useState } from "react";
 import { Weather } from "./components/Weather";
 export default function App() {
-    const [name, setName] = useState();
-    const [cityName, setCityName] = useState();
-    const calling = (names) => {
-        setName(names);
-    };
-    const callingcity = (CC) => {
-        setCityName(CC);
-    };
-    console.log(cityName);
-    return (
-        <NativeRouter>
-            <SafeAreaView style={styles.container}>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={<Inputs eventer={calling} />}
-                    ></Route>
-                    <Route
-                        path="/countrylist"
-                        element={
-                            <CountryList data={name} CC={callingcity} />
-                        }
-                    ></Route>
-                    <Route
-                        path="/weather"
-                        element={<Weather captialName={cityName} />}
-                    ></Route>
-                </Routes>
-                
-            </SafeAreaView>
-        </NativeRouter>
-    );
+  const [name, setName] = useState();
+  const [cityName, setCityName] = useState();
+  const calling = (names) => {
+    setName(names);
+  };
+  const callingcity = (CC) => {
+    setCityName(CC);
+  };
+  console.log(cityName);
+  return (
+    <NativeRouter>
+      <SafeAreaView style={styles.container}>
+        <Routes>
+          <Route path="/" element={<Inputs eventer={calling} />}></Route>
+          <Route
+            path="/countrylist"
+            element={<CountryList data={name} CC={callingcity} />}
+          ></Route>
+          <Route
+            path="/weather"
+            element={<Weather captialName={cityName} />}
+          ></Route>
+        </Routes>
+      </SafeAreaView>
+    </NativeRouter>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#4A47A3",
-        marginTop: 30,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#EBEAED",
+    marginTop: 20,
+  },
 });
--- components/input.tsx
+++ components/input.tsx
@@ -1,76 +1,74 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import {
-    TextInput,
-    StyleSheet,
-    View,
-    TouchableOpacity,
-    Text,
-    Alert,
+  TextInput,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+  Alert,
+  Button,
+  ImageBackgroundBase,
 } from "react-native";
 import { Link, useNavigate } from "react-router-native";
 
 export const Inputs = ({ eventer }) => {
-    const [country, setCountry] = useState();
-    const [disable, setDisable] = useState(false);
-    const navigate = useNavigate();
-    const NameList = () => {
-        if (country === undefined || country === "") {
-            setDisable(true);
-            Alert.alert("Please type country name here");
-        } else {
-            console.log("hello");
-            eventer(country);
-            navigate("/countrylist");
-        }
-    };
-    console.log(country);
-    const disablebutton = (e) => {
-        setCountry(e);
-        setDisable(false);
-    };
-    return (
-        <>
-            <TextInput
-                onChangeText={disablebutton}
-                style={styles.main}
-                placeholder={"Enter Country Name"}
-            />
+  const [country, setCountry] = useState();
+  const [disable, setDisable] = useState(false);
+  const navigate = useNavigate();
+  const NameList = () => {
+    if (country === undefined || country === "") {
+      setDisable(true);
+      Alert.alert("Please type country name here");
+    } else {
+      console.log("hello");
+      eventer(country);
+      navigate("/countrylist");
+    }
+  };
+  console.log(country);
+  const disablebutton = (e) => {
+    setCountry(e);
+    setDisable(false);
+  };
 
-            <TouchableOpacity onPress={NameList}>
-                <View style={styles.box}>
-                    <Text style={styles.button}>Click me</Text>
-                </View>
-            </TouchableOpacity>
-        </>
-    );
+  return (
+    <>
+      <TextInput
+        onChangeText={disablebutton}
+        style={styles.main}
+        placeholder={"Enter Country Name"}
+      />
+
+      <TouchableOpacity onPress={NameList}>
+        <View style={styles.box}>
+          <Text style={styles.btn1}>Click me</Text>
+        </View>
+      </TouchableOpacity>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-    main: {
-    
-        borderWidth: 2,
-        borderRadius: 20,
-        borderColor: "darkgrey",
-        padding: 12,
-        fontSize: 30,
-        marginTop: 20,
-        color:"black"
-    },
-    box: {
-        alignItems: "center",
-        marginTop: 30,
-        backgroundColor: "black",
-        padding: 12,
-        borderRadius: 50,
-        borderWidth: 2,
-        borderColor:"white"
-    },
-    button: {
-        color: "white",
-        fontWeight: "bold",
-        fontSize:20,
-        
-        
-    },
+  main: {
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "grey",
+    padding: 10,
+    fontSize: 20,
+    marginTop: 40,
+  },
+  box: {
+    alignItems: "center",
+    marginTop: 20,
+    backgroundColor: "#6750a4",
+    padding: 10,
+    borderRadius: 60,
+  },
+  btn1: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
 });
